77b03a70468c6e5e4937c95609e29663cd7a9d68
Steve Powell <steve.powell@edgewordstraining.co.uk>
2019-01-22 13:37:24 +0000

    Initial Commit

---
+++ pdftest/DumpOpenedWordDoc.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Created by Ranorex
+ * User: edgewords
+ * Date: 09/01/2019
+ * Time: 17:34
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+using Word = Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+
+namespace pdftest
+{
+    /// <summary>
+    /// Description of DumpOpenedWordDoc.
+    /// </summary>
+    [TestModule("5A322B94-16AC-42DE-9621-1FBD81A70E78", ModuleType.UserCode, 1)]
+    public class DumpOpenedWordDoc : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public DumpOpenedWordDoc()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+            
+            
+//			Word.Application word = null;
+//			try
+//			{
+//			  word = (Word.Application)Marshal.GetActiveObject("Word.Application");
+//			}
+//			catch (COMException)
+//			{
+//			}
+//			if (word == null) word = new Microsoft.Office.Interop.Word.Application();
+//			if(word == null) { /* report error */ }
+//			
+//			Ranorex.Report.Info("The current document is :" + word.ActiveDocument.FullName.ToString());
+            
+            
+//            Microsoft.Office.Interop.Word.Application WordObj;
+//			WordObj = (Microsoft.Office.Interop.Word.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Word.Application");
+//			for (int i = 0; i < WordObj.Windows.Count; i++)
+//			{
+//			    object idx = i + 1;
+//			    var WinObj = WordObj.Windows.get_Item(ref idx);
+//			    Ranorex.Report.Info(WinObj.Document.FullName);
+//			    
+//			}
+			
+            
+			//Get handle to already opened Word doc using marshal service. Both Word & Ranorex must be
+			//opened with Administrator permissions
+//            
+//			Word.Application objWord = System.Runtime.InteropServices.Marshal.GetActiveObject("Word.Application") as Word.Application;
+//			
+//			string docname = objWord.ActiveDocument.FullName;
+//			
+//			Ranorex.Report.Info(docname);
+
+
+			//Get handle on exiting opened Word Doc (need to know name and path in advance)
+			Word.Application objWord = new Microsoft.Office.Interop.Word.Application();
+			object missing = System.Reflection.Missing.Value;
+			Microsoft.Office.Interop.Word.Document document = objWord.Documents.Open(@"C:\Users\edgewords\Documents\ChatLog Meet Now 2019_01_08 22_00.rtf", false, true, false, ref missing, ref missing, false);
+			string docname = objWord.ActiveDocument.FullName;
+			
+			Ranorex.Report.Info(docname);
+			
+
+        }
+        
+        
+    }
+}
---
+++ pdftest/UserCodeModule1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Forms.Fields;
+using iText.Forms;
+using iText.Forms.Util;
+using iText.Forms.Xfa;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Linq;
+
+using Microsoft.Office.Interop.Word;
+
+
+namespace pdftest
+{
+    /// <summary>
+    /// Description of UserCodeModule1.
+    /// </summary>
+    [TestModule("CBCBA0F2-CC01-474A-B1F6-EB67BC17AB56", ModuleType.UserCode, 1)]
+    public class UserCodeModule1 : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public UserCodeModule1()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+            
+            // This is dependent on the itext 7 community NuGet package reference
+            
+            string src = @"C:\Users\edgewords\Documents\czechexamples\Property_contract_sample.pdf";
+           
+            //Read PDF
+            PdfReader reader = new PdfReader(src);
+            PdfDocument pdf = new PdfDocument(reader);
+            string text = string.Empty;
+ 
+            //Iterate over all pages
+            for(int page = 1; page <= pdf.GetNumberOfPages(); page++)
+            {
+            	PdfPage myPage = pdf.GetPage(page);
+            	//Extract plain text
+        		text += PdfTextExtractor.GetTextFromPage(myPage); 
+            }
+            reader.Close();
+            
+            //Find specific text
+            int pos = text.IndexOf("Spoluúčast")+10;
+            //Get next 10 characters
+            Ranorex.Report.Info (text.Substring(pos,10));
+            
+            
+            
+            //*************************//
+            // Now for the word document. Open the word document, then save as text only, you could then open it and read from it
+           	// using normal System.IO library
+           	
+        	Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
+            Document document = null; 
+            
+    		application.Visible = false;
+    		object missing = System.Reflection.Missing.Value;
+            object readOnly = false;
+            document = application.Documents.Open(@"C:\Users\edgewords\Documents\czechexamples\Property_policy_sample.doc", ref missing, ref readOnly);
+            //document.ExportAsFixedFormat(@"C:\Users\edgewords\Documents\czechexamples\Property_policy_sample.pdf", WdExportFormat.wdExportFormatPDF);
+            document.SaveAs(@"C:\Users\edgewords\Documents\czechexamples\Property_policy_sample.txt",2);
+            Ranorex.Report.Info(document.Range(1,document.Characters.Count).Text);
+            application.ActiveDocument.Close();
+            application.Quit();
+                
+
+
+			// Try to open the Xfa dynamic acrobat form as xml document
+			
+			// https://csharp.hotexamples.com/examples/iTextSharp.text.pdf/XfaForm/-/php-xfaform-class-examples.html
+			
+		
+            PdfReader newreader = new PdfReader(@"C:\Users\edgewords\Documents\czechexamples\eForm_example.pdf");
+            PdfDocument pdfDoc = new PdfDocument(newreader);
+            
+            XfaForm xfa = new XfaForm (pdfDoc);
+            if (xfa.IsXfaPresent()){
+            	
+            	XDocument myXMLDoc = xfa.GetDomDocument();
+            	IEnumerable<XElement> mylist =myXMLDoc.Descendants("CONTRACT_ID");
+            	List<XElement> mylistt = mylist.ToList<XElement>();
+            	Report.Info (mylistt[0].Value);
+            	
+            	// or if more than one, you can cycle through:
+            	/***
+            	foreach (XElement theEl in mylist){
+            		Report.Info(theEl.Value);
+            	}
+
+				***/
+            }
+            
+            else{
+            
+	            PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, false);
+				IDictionary<String, PdfFormField> fields = form.GetFormFields();
+				PdfFormField toSet;
+				fields.TryGetValue("CONTRACT_ID", out toSet);
+				
+				Report.Info(toSet.GetValueAsString());
+            }
+            
+		      //reader.Close();
+
+        }
+    }
+}
